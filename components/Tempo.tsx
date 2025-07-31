183036ed11324a0ed053d757f94d3a150249776d
unknown <linconkkjj@gmail.com>
2025-07-30 22:41:18 -0300

    tung tung tung sahur

--- components/Tempo.tsx
+++ components/Tempo.tsx
@@ -1,4 +1,4 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
  interface propsTempo{
         date:string,
@@ -10,6 +10,20 @@ import {View, Text, StyleSheet} from 'react-native';
 
 export default function Tempo({date, max, min, description, condition } : propsTempo){
 
+    const Icones: Record<string, any> = {
+        clear_day: require('../assets/png/clear_day.png'),
+        clear_night: require('../assets/png/clear_night.png'),
+        cloud: require('../assets/png/cloud.png'),
+        cloudly_day: require('../assets/png/cloudly_day.png'),
+        cloudly_night: require('../assets/png/cloudly_night.png'),
+        fog: require('../assets/png/fog.png'),
+        hail: require('../assets/png/hail.png'),
+        none_day: require('../assets/png/none_day.png'),
+        rain: require('../assets/png/rain.png'),
+        snow: require('../assets/png/snow.png'),
+        storm: require('../assets/png/storm.png'),
+    }
+
     return(
         <View style={styles.principal}>
                 <Text style={styles.texto}>Previsão para o dia: {date}</Text>
@@ -17,10 +31,19 @@ export default function Tempo({date, max, min, description, condition } : propsT
                 <Text style={styles.texto}>Mín: {min}</Text>
                 <Text style={styles.texto}>Descrição: {description}</Text>
                 <Text style={styles.texto}>Cond: {condition}</Text>
+                <Image
+                    style={styles.imagem}
+                    source={Icones[condition]}
+                    />
         </View>
     );
 }
 const styles = StyleSheet.create({
+    imagem:{
+        width:80,
+        height:80,
+    },
+    
     principal:{
         width:'80%',
         borderWidth:2,
