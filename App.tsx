abf0e6f79482cb3686dec49ed6dcc193d0b4f9bf
Linconwzx <linconkkjj@gmail.com>
2025-08-13 21:51:01 -0300

    concluido

--- App.tsx
+++ App.tsx
@@ -4,11 +4,17 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Home from './views/Home';
 import Itanhaem from './views/Itanhaem';
+import JoaoAmaro from './views/JoaoAmaro.';
+import SaoPaulo from './views/SaoPaulo';
+import Galinhos from './views/Galinhos';
 
 const RootStack = createNativeStackNavigator({
   screens: {
     Home: Home,
-    Itanhaem:Itanhaem
+    Itanhaem:Itanhaem,
+    JoaoAmaro:JoaoAmaro,
+    SaoPaulo:SaoPaulo,
+    Galinhos:Galinhos
   },
 });
 
